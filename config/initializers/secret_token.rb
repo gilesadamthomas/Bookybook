b89d9a6259819abe3d2baa872f7bb60f4a3b9aae
Giles Adam Thomas <giles@gilesadamthomas.com>
2012-08-26 00:26:05 +0100

    Totally uploading shit to github from Rails I rock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facefuck::Application.config.secret_token = '35b792c1dcf596e8fcb8fda78d83beaed20b66c734070c10b547354fd17eadd16c2c37dac7e57738f16f948af43fb83ce3d17ced33d71bac8a7f5005b6105d60'
